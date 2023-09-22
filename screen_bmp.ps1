f3aede207ee1a34f22f02e00b2ae2152db1c7904
blackmam <32873302+blackmam@users.noreply.github.com>
2023-09-22 08:43:40 +0200

    Add files via upload

---
+++ screen_bmp.ps1
@@ -0,0 +1,11 @@
+﻿[void][reflection.assembly]::loadwithpartialname("system.windows.forms")
+$MyScreenshot = [System.Windows.Forms.SystemInformation]::VirtualScreen
+$WID = $MyScreenshot.Width
+$HEI = $MyScreenshot.Height
+$LEFT = $MyScreenshot.Left
+$TOP = $MyScreenshot.Top
+$MyBitmap = New-Object System.Drawing.Bitmap $WID, $HEI
+$MyDrawing = [System.Drawing.Graphics]::FromImage($MyBitmap)
+$MyDrawing.CopyFromScreen($LEFT, $TOP, 0, 0, $MyBitmap.Size)
+$MyFile = 'c:\test.bmp'
+$MyBitmap.Save($MyFile)
